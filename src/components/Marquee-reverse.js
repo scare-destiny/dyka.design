334aa1078057af5701f471b5d6da491cfd35d896
scare-destiny <zhenya.venger@gmail.com>
2022-08-29 09:17:11 +0300

    Updated reverse marqueese

--- src/components/Marquee-reverse.js
+++ src/components/Marquee-reverse.js
@@ -3,6 +3,11 @@ import image7 from '../assets/gallery-7.jpg'
 import image8 from '../assets/gallery-8.jpg'
 import image9 from '../assets/gallery-9.jpg'
 import image10 from '../assets/gallery-10.jpg'
+import image6low from '../assets/lowres/gallery-6-lowres.jpg';
+import image7low from '../assets/lowres/gallery-7-lowres.jpg';
+import image8low from '../assets/lowres/gallery-8-lowres.jpg';
+import image9low from '../assets/lowres/gallery-9-lowres.jpg';
+import image10low from '../assets/lowres/gallery-10-lowres.jpg';
 
 const MarqueeReversed = () => {
   return (
@@ -10,45 +15,55 @@ const MarqueeReversed = () => {
       <div className="marquee marquee--reverse">
         <div className="marquee__group">
           <img
-            src={image6}
+            src={image6low}
+            srcSet={image6}
             alt="logo"
           />
           <img
-            src={image7}
+            src={image7low}
+            srcSet={image7}
             alt="logo"
           />
           <img
-            src={image8}
+            src={image8low}
+            srcSet={image8}
             alt="logo"
           />
           <img
-            src={image9}
+            src={image9low}
+            srcSet={image9}
             alt="logo"
 					/>
-					 <img
-            src={image10}
+          <img
+            src={image10low}
+            srcSet={image10}
             alt="logo"
           />
         </div>
         <div aria-hidden="true" className="marquee__group">
-				<img
-            src={image6}
+          <img
+            src={image6low}
+            srcSet={image6}
             alt="logo"
           />
           <img
-            src={image7}
+            src={image7low}
+            srcSet={image7}
             alt="logo"
           />
           <img
-            src={image8}
+            src={image8low}
+            srcSet={image8}
             alt="logo"
           />
           <img
-            src={image9}
+            src={image9low}
+            srcSet={image9}
             alt="logo"
 					/>
-					 <img
-            src={image10}
+          <img
+            src={image10low}
+            srcSet={image10}
             alt="logo"
           />
         </div>
