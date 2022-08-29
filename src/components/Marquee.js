389a3c0354e0125a87f2fac3bddd4e158d357ec4
scare-destiny <zhenya.venger@gmail.com>
2022-08-29 09:01:03 +0300

    Added srcSet for gallery

--- src/components/Marquee.js
+++ src/components/Marquee.js
@@ -4,6 +4,11 @@ import image2 from '../assets/gallery-2.jpg'
 import image3 from '../assets/gallery-3.jpg'
 import image4 from '../assets/gallery-4.jpg'
 import image5 from '../assets/gallery-5.jpg'
+import image1low from '../assets/lowres/gallery-1-lowres.jpg';
+import image2low from '../assets/lowres/gallery-2-lowres.jpg';
+import image3low from '../assets/lowres/gallery-3-lowres.jpg';
+import image4low from '../assets/lowres/gallery-4-lowres.jpg';
+import image5low from '../assets/lowres/gallery-5-lowres.jpg';
 
 const Marquee = () => {
   return (
@@ -11,45 +16,55 @@ const Marquee = () => {
       <div className="marquee">
         <div className="marquee__group">
           <img
-            src={image1}
+            src={image1low}
+            srcSet={image1}
             alt=""
           />
           <img
-            src={image2}
+            src={image2low}
+            srcSet={image2}
             alt=""
           />
           <img
-            src={image3}
+            src={image3low}
+            srcSet={image3}
             alt=""
           />
           <img
-            src={image4}
+            src={image4low}
+            srcSet={image4}
             alt=""
           />
           <img
-            src={image5}
+            src={image5low}
+            srcSet={image5}
             alt=""
           />
         </div>
         <div aria-hidden="true" className="marquee__group">
 				<img
-            src={image1}
+            src={image1low}
+            srcSet={image2}
             alt=""
           />
           <img
-            src={image2}
+            src={image2low}
+            srcSet={image2}
             alt=""
           />
           <img
-            src={image3}
+            src={image3low}
+            srcSet={image3}
             alt=""
           />
           <img
-            src={image4}
+            src={image4low}
+            srcSet={image4}
             alt=""
           />
           <img
-            src={image5}
+            src={image5low}
+            srcSet={image5}
             alt=""
           />
         </div>
