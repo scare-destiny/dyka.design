a68a593765914e357a6987113ec4c1c1a0dc958f
scare-destiny <zhenya.venger@gmail.com>
2022-08-29 10:18:47 +0300

    responsiveness

--- src/components/Marquee-reverse.js
+++ src/components/Marquee-reverse.js
@@ -1,74 +1,74 @@
-import image6 from '../assets/gallery-6.jpg'
-import image7 from '../assets/gallery-7.jpg'
-import image8 from '../assets/gallery-8.jpg'
-import image9 from '../assets/gallery-9.jpg'
-import image10 from '../assets/gallery-10.jpg'
-import image6low from '../assets/lowres/gallery-6-lowres.jpg';
-import image7low from '../assets/lowres/gallery-7-lowres.jpg';
-import image8low from '../assets/lowres/gallery-8-lowres.jpg';
-import image9low from '../assets/lowres/gallery-9-lowres.jpg';
-import image10low from '../assets/lowres/gallery-10-lowres.jpg';
+import image6 from "../assets/gallery-6.jpg";
+import image7 from "../assets/gallery-7.jpg";
+import image8 from "../assets/gallery-8.jpg";
+import image9 from "../assets/gallery-9.jpg";
+import image10 from "../assets/gallery-10.jpg";
+import image6low from "../assets/lowres/gallery-6-lowres.jpg";
+import image7low from "../assets/lowres/gallery-7-lowres.jpg";
+import image8low from "../assets/lowres/gallery-8-lowres.jpg";
+import image9low from "../assets/lowres/gallery-9-lowres.jpg";
+import image10low from "../assets/lowres/gallery-10-lowres.jpg";
 
 const MarqueeReversed = () => {
   return (
-   <>
+    <>
       <div className="marquee marquee--reverse">
         <div className="marquee__group">
           <img
-             srcSet={`${image6low} 200w, ${image6} 1000w`}
-            src={image6low}
+            src={image6}
+            srcSet={`${image6low} 480W, ${image6} 1000w`}
             alt="logo"
           />
           <img
-             srcSet={`${image7low} 200w, ${image7} 1000w`}
-            src={image7low}
+            src={image7}
+            srcSet={`${image7low} 480W, ${image7} 1000w`}
             alt="logo"
           />
           <img
-             srcSet={`${image8low} 200w, ${image8} 1000w`}
-            src={image8low}
+            src={image8}
+            srcSet={`${image8low} 480W, ${image8} 1000w`}
             alt="logo"
           />
           <img
-             srcSet={`${image9low} 200w, ${image9} 1000w`}
-            src={image9low}
+            src={image9}
+            srcSet={`${image9low} 480W, ${image9} 1000w`}
             alt="logo"
-					/>
+          />
           <img
-             srcSet={`${image10low} 200w, ${image10} 1000w`}
-            src={image10low}
+            src={image10}
+            srcSet={`${image10low} 480W, ${image10} 1000w`}
             alt="logo"
           />
         </div>
         <div aria-hidden="true" className="marquee__group">
           <img
-             srcSet={`${image6low} 200w, ${image6} 1000w`}
-            src={image6low}
+            src={image6}
+            srcSet={`${image6low} 480W, ${image6} 1000w`}
             alt="logo"
           />
           <img
-            srcSet={`${image7low} 200w, ${image7} 1000w`}
-            src={image7low}
+            src={image7}
+            srcSet={`${image7low} 480W, ${image7} 1000w`}
             alt="logo"
           />
           <img
-             srcSet={`${image8low} 200w, ${image8} 1000w`}
-            src={image8low}
+           src={image8}
+           srcSet={`${image8low} 480W, ${image8} 1000w`}
+           alt="logo"
+          />
+          <img
+            src={image9}
+            srcSet={`${image9low} 480W, ${image9} 1000w`}
             alt="logo"
           />
           <img
-            srcSet={`${image9low} 200w, ${image9} 1000w`}
-            src={image9low}
-            alt="logo"
-					/>
-          <img
-             srcSet={`${image10low} 200w, ${image10} 1000w`}
-            src={image10low}
+            src={image10}
+            srcSet={`${image10low} 480W, ${image10} 1000w`}
             alt="logo"
           />
         </div>
       </div>
-			</>
+    </>
   );
 };
 
