75c21262b30e26c6f9566412bc3c540f76d99849
scare-destiny <zhenya.venger@gmail.com>
2022-09-14 16:15:14 +0300

    added lazy loading to marqueese images

--- src/components/Marquee/Marquee-reverse.js
+++ src/components/Marquee/Marquee-reverse.js
@@ -1,75 +1,95 @@
-import image6 from "../../assets/gallery-6.webp";
-import image7 from "../../assets/gallery-7.webp";
-import image8 from "../../assets/gallery-8.webp";
-import image9 from "../../assets/gallery-9.webp";
-import image10 from "../../assets/gallery-10.webp";
-import image6low from "../../assets/lowres/gallery-6-lowres.webp";
-import image7low from "../../assets/lowres/gallery-7-lowres.webp";
-import image8low from "../../assets/lowres/gallery-8-lowres.webp";
-import image9low from "../../assets/lowres/gallery-9-lowres.webp";
-import image10low from "../../assets/lowres/gallery-10-lowres.webp";
+import image6 from '../../assets/gallery-6.webp'
+import image7 from '../../assets/gallery-7.webp'
+import image8 from '../../assets/gallery-8.webp'
+import image9 from '../../assets/gallery-9.webp'
+import image10 from '../../assets/gallery-10.webp'
+import image6low from '../../assets/lowres/gallery-6-lowres.webp'
+import image7low from '../../assets/lowres/gallery-7-lowres.webp'
+import image8low from '../../assets/lowres/gallery-8-lowres.webp'
+import image9low from '../../assets/lowres/gallery-9-lowres.webp'
+import image10low from '../../assets/lowres/gallery-10-lowres.webp'
 
 const MarqueeReversed = () => {
-  return (
-    <>
-      <div className="marquee marquee--reverse">
-        <div className="marquee__group">
-          <img
-             src={image6low}
-             srcSet={`${image6} 1800w, ${image6low} 1200w`} 
-            alt="logo"
-          />
-          <img
-              src={image7low}
-              srcSet={`${image7} 1800w, ${image7low} 900w`} 
-            alt="logo"
-          />
-          <img
-              src={image8low}
-              srcSet={`${image8} 1800w, ${image8low} 1200w`} 
-            alt="logo"
-          />
-          <img
-             src={image9low}
-             srcSet={`${image9} 1800w, ${image9low} 1200w`} 
-            alt="logo"
-          />
-          <img
-              src={image10low}
-              srcSet={`${image10} 1800w, ${image10low} 1200w`} 
-            alt="logo"
-          />
-        </div>
-        <div aria-hidden="true" className="marquee__group">
-          <img
-              src={image6low}
-              srcSet={`${image6} 1800w, ${image6low} 1200w`} 
-            alt="logo"
-          />
-          <img
-              src={image7low}
-              srcSet={`${image7} 1800w, ${image7low} 1200w`} 
-            alt="logo"
-          />
-          <img
-             src={image8low}
-             srcSet={`${image8} 1800w, ${image8low} 1200w`} 
-           alt="logo"
-          />
-          <img
-              src={image9low}
-              srcSet={`${image9} 1800w, ${image9low} 1200w`} 
-            alt="logo"
-          />
-          <img
-              src={image10low}
-              srcSet={`${image10} 1800w, ${image10low} 1200w`} 
-            alt="logo"
-          />
-        </div>
-      </div>
-    </>
-  );
-};
+	return (
+		<>
+			<div className='marquee marquee--reverse'>
+				<div className='marquee__group'>
+					<img
+						src={image6low}
+						srcSet={`${image6} 1800w, ${image6low} 1200w`}
+						alt='logo'
+						decoding='async'
+						loading='lazy'
+					/>
+					<img
+						src={image7low}
+						srcSet={`${image7} 1800w, ${image7low} 900w`}
+						alt='logo'
+						decoding='async'
+						loading='lazy'
+					/>
+					<img
+						src={image8low}
+						srcSet={`${image8} 1800w, ${image8low} 1200w`}
+						alt='logo'
+						decoding='async'
+						loading='lazy'
+					/>
+					<img
+						src={image9low}
+						srcSet={`${image9} 1800w, ${image9low} 1200w`}
+						alt='logo'
+						decoding='async'
+						loading='lazy'
+					/>
+					<img
+						src={image10low}
+						srcSet={`${image10} 1800w, ${image10low} 1200w`}
+						alt='logo'
+						decoding='async'
+						loading='lazy'
+					/>
+				</div>
+				<div aria-hidden='true' className='marquee__group'>
+					<img
+						src={image6low}
+						srcSet={`${image6} 1800w, ${image6low} 1200w`}
+						alt='logo'
+						decoding='async'
+						loading='lazy'
+					/>
+					<img
+						src={image7low}
+						srcSet={`${image7} 1800w, ${image7low} 1200w`}
+						alt='logo'
+						decoding='async'
+						loading='lazy'
+					/>
+					<img
+						src={image8low}
+						srcSet={`${image8} 1800w, ${image8low} 1200w`}
+						alt='logo'
+						decoding='async'
+						loading='lazy'
+					/>
+					<img
+						src={image9low}
+						srcSet={`${image9} 1800w, ${image9low} 1200w`}
+						alt='logo'
+						decoding='async'
+						loading='lazy'
+					/>
+					<img
+						src={image10low}
+						srcSet={`${image10} 1800w, ${image10low} 1200w`}
+						alt='logo'
+						decoding='async'
+						loading='lazy'
+					/>
+				</div>
+			</div>
+		</>
+	)
+}
 
-export default MarqueeReversed;
+export default MarqueeReversed
--- src/components/Marquee/Marquee.js
+++ src/components/Marquee/Marquee.js
@@ -1,76 +1,94 @@
-import React from "react";
-import image1 from "../../assets/gallery-1.webp";
-import image2 from "../../assets/gallery-2.webp";
-import image3 from "../../assets/gallery-3.webp";
-import image4 from "../../assets/gallery-4.webp";
-import image5 from "../../assets/gallery-5.webp";
-import image1low from "../../assets/lowres/gallery-1-lowres.webp";
-import image2low from "../../assets/lowres/gallery-2-lowres.webp";
-import image3low from "../../assets/lowres/gallery-3-lowres.webp";
-import image4low from "../../assets/lowres/gallery-4-lowres.webp";
-import image5low from "../../assets/lowres/gallery-5-lowres.webp";
+import React from 'react'
+import image1 from '../../assets/gallery-1.webp'
+import image2 from '../../assets/gallery-2.webp'
+import image3 from '../../assets/gallery-3.webp'
+import image4 from '../../assets/gallery-4.webp'
+import image5 from '../../assets/gallery-5.webp'
+import image1low from '../../assets/lowres/gallery-1-lowres.webp'
+import image2low from '../../assets/lowres/gallery-2-lowres.webp'
+import image3low from '../../assets/lowres/gallery-3-lowres.webp'
+import image4low from '../../assets/lowres/gallery-4-lowres.webp'
+import image5low from '../../assets/lowres/gallery-5-lowres.webp'
 
 const Marquee = () => {
-  return (
-    <>
-      <div className="marquee">
-        <div className="marquee__group">
-          <img
-            src={image1low}
-            srcSet={`${image1} 1800w, ${image1low} 1200w`} 
-            alt=""
-          />
-          <img
-             src={image2low}
-             srcSet={`${image2} 1800w, ${image2low} 1200w`} 
-            alt=""
-          />
-          <img
-             src={image3low}
-             srcSet={`${image3} 1800w, ${image3low} 1200w`} 
-            alt=""
-          />
-          <img
-            src={image4low}
-            srcSet={`${image4} 1800w, ${image4low} 1200w`} 
-            alt=""
-          />
-          <img
-              src={image5low}
-              srcSet={`${image5} 1800w, ${image5low} 1200w`} 
-            alt=""
-          />
-        </div>
-        <div aria-hidden="true" className="marquee__group">
-          <img
-              src={image1low}
-              srcSet={`${image1} 1800w, ${image1low} 1200w`} 
-            alt=""
-          />
-          <img
-              src={image2low}
-              srcSet={`${image2} 1800w, ${image2low} 1200w`} 
-            alt=""
-          />
-          <img
-              src={image3low}
-              srcSet={`${image3} 1800w, ${image3low} 1200w`} 
-            alt=""
-          />
-          <img
-             src={image4low}
-             srcSet={`${image4} 1800w, ${image4low} 1200w`} 
-            alt=""
-          />
-          <img
-             src={image5low}
-             srcSet={`${image5} 1800w, ${image5low} 1200w`} 
-            alt=""
-          />
-        </div>
-      </div>
-    </>
-  );
-};
+	return (
+		<>
+			<div className='marquee'>
+				<div className='marquee__group'>
+					<img
+						src={image1low}
+						srcSet={`${image1} 1800w, ${image1low} 1200w`}
+						alt=''
+					/>
+					<img
+						src={image2low}
+						srcSet={`${image2} 1800w, ${image2low} 1200w`}
+						alt=''
+						decoding='async'
+						loading='lazy'
+					/>
+					<img
+						src={image3low}
+						srcSet={`${image3} 1800w, ${image3low} 1200w`}
+						alt=''
+						decoding='async'
+						loading='lazy'
+					/>
+					<img
+						src={image4low}
+						srcSet={`${image4} 1800w, ${image4low} 1200w`}
+						alt=''
+						decoding='async'
+						loading='lazy'
+					/>
+					<img
+						src={image5low}
+						srcSet={`${image5} 1800w, ${image5low} 1200w`}
+						alt=''
+						decoding='async'
+						loading='lazy'
+					/>
+				</div>
+				<div aria-hidden='true' className='marquee__group'>
+					<img
+						src={image1low}
+						srcSet={`${image1} 1800w, ${image1low} 1200w`}
+						alt=''
+						decoding='async'
+						loading='lazy'
+					/>
+					<img
+						src={image2low}
+						srcSet={`${image2} 1800w, ${image2low} 1200w`}
+						alt=''
+						decoding='async'
+						loading='lazy'
+					/>
+					<img
+						src={image3low}
+						srcSet={`${image3} 1800w, ${image3low} 1200w`}
+						alt=''
+						decoding='async'
+						loading='lazy'
+					/>
+					<img
+						src={image4low}
+						srcSet={`${image4} 1800w, ${image4low} 1200w`}
+						alt=''
+						decoding='async'
+						loading='lazy'
+					/>
+					<img
+						src={image5low}
+						srcSet={`${image5} 1800w, ${image5low} 1200w`}
+						alt=''
+						decoding='async'
+						loading='lazy'
+					/>
+				</div>
+			</div>
+		</>
+	)
+}
 
-export default Marquee;
+export default Marquee
