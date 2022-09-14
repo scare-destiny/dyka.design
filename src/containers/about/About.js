2a477d7cdd8c480953e50f169f540158085a0895
scare-destiny <zhenya.venger@gmail.com>
2022-09-14 17:51:41 +0300

    added heading

--- src/containers/about/About.js
+++ src/containers/about/About.js
@@ -1,28 +1,72 @@
 import React from 'react'
+import {
+	Avatar,
+	Box,
+	chakra,
+	Flex,
+	Icon,
+	SimpleGrid,
+	useColorModeValue,
+} from '@chakra-ui/react'
 import './about.css'
 
 const About = () => {
 	return (
-		<div className='dyka__about'>
-			<div className='dyka__about-text_container section__padding'>
-				<p>
-					Hi, my name is Anastasiia, and welcome to my website! My passion for design
-					and understanding clients' needs is what I have lived and breathed the last
-					decade. For me, talent results from discipline, so I push myself every day
-					to improve.
-				</p>
-				<p>
+		<Flex
+			textAlign={'center'}
+			pt={10}
+			pb={10}
+			justifyContent={'center'}
+			direction={'column'}
+			width={'full'}
+		>
+			<div class='about-heading-wrapper'>
+				<div class='about-heading-container'>
+					<h1>
+						WELCOME <break />
+						TO
+						<break /> DYKA DESIGN
+					</h1>
+				</div>
+			</div>
+			<Box width={{ base: 'full', sm: 'lg', lg: 'xl' }} margin={'auto'}>
+				<chakra.p
+					fontFamily={'Inter'}
+					fontWeight={'medium'}
+					fontSize={'1rem'}
+					p={5}
+				>
+					<span className='about-span'>
+						{' '}
+						Hi, my name is Anastasiia, and welcome to my website!{' '}
+					</span>{' '}
+					My passion for design and understanding clients' needs is what I have lived
+					and breathed the last decade. For me, talent results from discipline, so I
+					push myself every day to improve.
+				</chakra.p>
+				<chakra.p
+					fontFamily={'Inter'}
+					fontWeight={'medium'}
+					fontSize={'1rem'}
+					p={5}
+				>
 					My career started at a design bureau while I studied at Berlin
 					International University. Over the years, I've successfully finished dozens
 					of projects and received positive feedback.
-				</p>
-				<p>
+				</chakra.p>
+				<chakra.p
+					fontFamily={'Inter'}
+					fontWeight={'medium'}
+					fontSize={'1rem'}
+					p={5}
+				>
 					Every interior should tell your authentic story. It should express where
 					you've been, where you are, and where you're going. I take pride in helping
 					my clients live life more beautifully and turn their ideas into reality.
-				</p>
-			</div>
-		</div>
+				</chakra.p>
+			</Box>
+		</Flex>
+		// </div>
 	)
 }
 
